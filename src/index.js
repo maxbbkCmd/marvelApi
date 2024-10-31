956e673ad9dd2dc611f4320c6cdd63cb2bc4c58c
maxbbkCmd <maxbbkCmd@mail.ru>
2024-10-31 16:38:06 +0300

    Data transformation and a component with a random character

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
-import MarvelService from './services/MarvelService';
+// import MarvelService from './services/MarvelService';
 
-const marvelService = new MarvelService();
-marvelService.getAllCharacter().then((res) => res.data.results.forEach((item) => console.log(item.id)));
+// const marvelService = new MarvelService();
+// marvelService.getAllCharacter().then((res) => res.data.results.forEach((item) => console.log(item.id)));
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
-
-
